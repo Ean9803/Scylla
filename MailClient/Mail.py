4292fb6f143f96d462dc1ea64feb2b6989f8f2b3
Ian <irobot9803@gmail.com>
2023-10-23 00:51:34 -0500

    smtp mail client done

--- MailClient/Mail.py
+++ MailClient/Mail.py
@@ -1,38 +1,73 @@
 from socket import *
-msg = "\r\n I love computer networks!"
+import base64
+import ssl
+
 endmsg = "\r\n.\r\n"
-# Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver = #Fill in start #Fill in end
-# Create socket called clientSocket and establish a TCP connection with mailserver
-#Fill in start 
-#Fill in end
-recv = clientSocket.recv(1024).decode()
-print(recv)
+mailserver = ("smtp.gmail.com", 465) #Fill in start #Fill in end
+Socket = socket(AF_INET, SOCK_STREAM)
+Socket.settimeout(1)
+clientSocket = ssl.wrap_socket(Socket)
+clientSocket.connect(mailserver)
+recv = clientSocket.recv(1024)
+recv = recv.decode()
+print("Message after connection request:" + recv)
 if recv[:3] != '220':
     print('220 reply not received from server.')
-# Send HELO command and print server response.
-heloCommand = 'HELO Alice\r\n'
+heloCommand = 'EHLO Alice\r\n'
 clientSocket.send(heloCommand.encode())
-recv1 = clientSocket.recv(1024).decode()
-print(recv1)
+recv1 = clientSocket.recv(1024)
+recv1 = recv1.decode()
+print("Message after EHLO command:" + recv1)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
- 
+
+username =  "vs2022test2023@gmail.com"
+key = "vfhr xprb sexh opwy"
+base64_str = ("\x00"+username+"\x00"+key).encode()
+base64_str = base64.b64encode(base64_str)
+authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
+clientSocket.send(authMsg)
+recv_auth = clientSocket.recv(1024)
+print(recv_auth.decode())
+
+
 # Send MAIL FROM command and print server response.
-# Fill in start
-# Fill in end
-# Send RCPT TO command and print server response. 
-# Fill in start
-# Fill in end
-# Send DATA command and print server response. 
-# Fill in start
-# Fill in end
+mailFrom = "MAIL FROM: <vs2022test2023@gmail.com> \r\n"
+clientSocket.send(mailFrom.encode())
+recv2 = clientSocket.recv(1024).decode()
+print("After MAIL FROM command: " + recv2)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send RCPT TO command and print server response.
+rcptTo = "RCPT TO: <irobot9803@gmail.com> \r\n"
+clientSocket.send(rcptTo.encode())
+recv3 = clientSocket.recv(1024).decode()
+print("After RCPT TO command: " + recv3)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send DATA command and print server response.
+data = "DATA\r\n"
+clientSocket.send(data.encode())
+recv4 = clientSocket.recv(1024).decode()
+print("After DATA command: " + recv4)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
 # Send message data.
-# Fill in start
-# Fill in end
-# Message ends with a single period.
-# Fill in start
-# Fill in end
+subject = "Subject: SMTP mail client testing \r\n\r\n" 
+clientSocket.send(subject.encode())
+message = input("Enter your message: \r\n")
+clientSocket.send(message.encode())
+clientSocket.send(endmsg.encode())
+recv_msg = clientSocket.recv(1024)
+print("Response after sending message body:" + recv_msg.decode())
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
 # Send QUIT command and get server response.
-# Fill in start
-# Fill in end
+clientSocket.send("QUIT\r\n".encode())
+message=clientSocket.recv(1024).decode()
+print (message)
+clientSocket.close()
